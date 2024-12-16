96ff2ff9b61b7803691818cb332473c7b74ccf77
muhammad_wasim <muhammad.wasim@starloopstudios.com>
2024-12-16 21:41:28 +0500

    Plugin import is in progress

--- Source/TestWebIntegration/TestWebIntegration.Build.cs
+++ Source/TestWebIntegration/TestWebIntegration.Build.cs
@@ -8,7 +8,7 @@ public class TestWebIntegration : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Json", "JsonUtilities","VorbisAudioDecoder" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Json", "JsonUtilities"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
